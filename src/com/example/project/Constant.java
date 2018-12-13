cba0b504f206e0dc5ca877857c4180fb4f071f6b
George Xu <11903552+karonx@users.noreply.github.com>
2018-12-14 01:37:47 +0800

    Update Constant.java

--- src/com/example/project/Constant.java
+++ src/com/example/project/Constant.java
@@ -1,5 +1,5 @@
 package com.example.project;
 
 public class Constant {
-	public static String BMOBID="5843776e79b8a88f84bd2de6ca851dbf";
+	public static String BMOBID="************************";
 }
